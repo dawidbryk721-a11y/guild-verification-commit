823e315af9dd2d8cb4bc161bee30429b7959153a
dawidbryk721-a11y <dawidbryk721@gmail.com>
2026-03-24 20:27:29 +0100

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T19:27:04.656Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-24T19:27:28.316Z
+Commit: Resolve null pointer exception in user service
