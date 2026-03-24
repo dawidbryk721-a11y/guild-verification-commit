a6f4168d4d3397fc2a2da1c22a472023715b9d86
dawidbryk721-a11y <dawidbryk721@gmail.com>
2026-03-24 20:31:39 +0100

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-24T19:31:27.676Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-24T19:31:38.114Z
+Commit: Patch security vulnerability in authentication
